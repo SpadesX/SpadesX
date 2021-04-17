96102fad921544f67761ae199381dda247dd26e0
Haxk20 <16738302+Haxk20@users.noreply.github.com>
2021-04-17 23:03:17 +0200

    server: Fix input data packet
    Seriously WTF was i drinking ?

--- Source/Server.c
+++ Source/Server.c
@@ -679,12 +679,13 @@ static void OnPacketReceived(GameServer* server, uint8 playerID, DataStream* dat
 
 static void SendInputData(GameServer* server, uint8 playerID)
 {
+	ENetPacket* packet = enet_packet_create(NULL, 3, ENET_PACKET_FLAG_RELIABLE);
+	DataStream  stream = {packet->data, packet->dataLength, 0};
+	WriteByte(&stream, PACKET_TYPE_INPUT_DATA);
+	WriteByte(&stream, playerID);
+	WriteByte(&stream, server->input[playerID]);
 	for (uint8 i = 0; i < 32; ++i) {
 		if (playerID != i && server->state[i] != STATE_DISCONNECTED) {
-			ENetPacket* packet = enet_packet_create(NULL, 1 + (32 * 24), ENET_PACKET_FLAG_UNSEQUENCED);
-			DataStream  stream = {packet->data, packet->dataLength, 0};
-			WriteByte(&stream, PACKET_TYPE_INPUT_DATA);
-			WriteByte(&stream, server->input[playerID]);
 			enet_peer_send(server->peer[i], 0, packet);
 		}
 	}
