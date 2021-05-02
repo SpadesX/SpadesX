9f94f54ff2175991c04f1ce750bd574352eb2521
Haxk20 <16738302+Haxk20@users.noreply.github.com>
2021-05-02 16:04:25 +0200

    all: Delete nearly all code. We are doing a rewrite

--- Source/Main.c
+++ Source/Main.c
@@ -1,11 +1,6 @@
-#include "Server.h"
-
 #include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
 
-int main()
-{
-    ServerRun(DEFAULT_SERVER_PORT, 32, 2, 0, 0);
-    return EXIT_SUCCESS;
+int main(int argc, char *argv[]) {
+	
+	return 0;
 }
