e88bf11a5367592913f1815b2e4e8e6e845f58b3
Haxk20 <16738302+Haxk20@users.noreply.github.com>
2021-05-11 13:59:38 +0200

    Protocol.c: I totally didnt use the wrong ID in last commit

--- Source/Protocol.c
+++ Source/Protocol.c
@@ -11,7 +11,7 @@
 uint8 playerToPlayerVisible(Server *server, uint8 playerID, uint8 playerID2) {
 	float distance = 0;
 	distance = sqrt(pow((server->player[playerID].pos.x - server->player[playerID2].pos.x), 2) + pow((server->player[playerID].pos.y - server->player[playerID2].pos.y), 2));
-	if (server->player[playerID2].team == TEAM_SPECTATOR) {
+	if (server->player[playerID].team == TEAM_SPECTATOR) {
 		return 1;
 	}
 	else if (distance >= 132) {
@@ -33,7 +33,7 @@ void SendPacketExceptSender(Server *server, ENetPacket* packet, uint8 playerID) 
 void SendPacketExceptSenderDistCheck(Server *server, ENetPacket* packet, uint8 playerID) {
 	for (uint8 i = 0; i < 32; ++i) {
 		if (playerID != i && server->player[i].state != STATE_DISCONNECTED) {
-			if (playerToPlayerVisible(server, playerID, i) || server->player[i].team == TEAM_SPECTATOR) {
+			if (playerToPlayerVisible(server, playerID, i)) {
 				enet_peer_send(server->player[i].peer, 0, packet);
 			}
 		}
