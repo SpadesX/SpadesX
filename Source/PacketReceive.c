0f62e8885e323b848a351967f628ae2f2b567c48
Haxk20 <16738302+Haxk20@users.noreply.github.com>
2021-09-01 19:50:23 +0200

    PacketReceive.c: Check if player is alive in Hit Packet
    This should disallow double kills or killing each other
    at the same time

--- Source/PacketReceive.c
+++ Source/PacketReceive.c
@@ -73,31 +73,32 @@ void ReceiveHitPacket(Server* server, uint8 playerID, uint8 hitPlayerID, uint8 h
     Vector3f shotOrien  = server->player[playerID].movement.forwardOrientation;
     float    distance   = DistanceIn2D(shotPos, hitPos);
     long     x, y, z;
-    if ((server->player[playerID].team != server->player[hitPlayerID].team ||
+    if (server->player[playerID].alive && server->player[hitPlayerID].alive &&
+        (server->player[playerID].team != server->player[hitPlayerID].team ||
          server->player[playerID].allowTeamKilling) &&
         (server->player[playerID].allowKilling && server->globalAK) && validate_hit(shotPos, shotOrien, hitPos, 5) &&
         (cast_ray(server,
-                 shotEyePos.x,
-                 shotEyePos.y,
-                 shotEyePos.z,
-                 shotOrien.x,
-                 shotOrien.y,
-                 shotOrien.z,
-                 distance,
-                 &x,
-                 &y,
-                 &z) == 0 ||
-        cast_ray(server,
-                 shotEyePos.x,
-                 shotEyePos.y,
-                 shotEyePos.z - 1,
-                 shotOrien.x,
-                 shotOrien.y,
-                 shotOrien.z,
-                 distance,
-                 &x,
-                 &y,
-                 &z) == 0))
+                  shotEyePos.x,
+                  shotEyePos.y,
+                  shotEyePos.z,
+                  shotOrien.x,
+                  shotOrien.y,
+                  shotOrien.z,
+                  distance,
+                  &x,
+                  &y,
+                  &z) == 0 ||
+         cast_ray(server,
+                  shotEyePos.x,
+                  shotEyePos.y,
+                  shotEyePos.z - 1,
+                  shotOrien.x,
+                  shotOrien.y,
+                  shotOrien.z,
+                  distance,
+                  &x,
+                  &y,
+                  &z) == 0))
     {
         switch (server->player[playerID].weapon) {
             case WEAPON_RIFLE:
