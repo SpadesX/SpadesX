bc88ab94cdb50766006a10cd75923afdfb2216bc
Haxk20 <16738302+Haxk20@users.noreply.github.com>
2021-10-22 21:19:28 +0200

    Packets.c: Beep boop me forgot debug comment boop beep

--- Source/Packets.c
+++ Source/Packets.c
@@ -1110,7 +1110,6 @@ static void receiveWeaponReload(Server* server, uint8 playerID, DataStream* data
         // 255. How am i supposed to check for hacks dammit with this kind of info dammit ?
     }
     SendWeaponReload(server, playerID);
-    printf("Clip: %d, Reserve: %d\n", server->player[playerID].weaponClip, server->player[playerID].weaponReserve);
 }
 
 static void receiveChangeTeam(Server* server, uint8 playerID, DataStream* data)
