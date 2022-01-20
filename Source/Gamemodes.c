56b481dffeef99808d1cb259b47693c0fdc5bb14
Haxk20 <16738302+Haxk20@users.noreply.github.com>
2022-01-20 23:55:36 +0100

    Gamemodes.c: Separate gamemodes into specific init functions
    Having each gamemode directly in the if statement would soon enough
    make for a messy code. So lets not go down that path from the start.

--- Source/Gamemodes.c
+++ Source/Gamemodes.c
@@ -1,54 +1,66 @@
 #include "Protocol.h"
 #include "Structs.h"
 
-#include <stdio.h>
 #include <math.h>
+#include <stdio.h>
 #include <string.h>
+static void initCTF(Server* server)
+{
+    memcpy(server->gamemodeName, "ctf", strlen("ctf") + 1);
+    // Init CTF
+    server->protocol.gameMode.score[0]   = 0;
+    server->protocol.gameMode.score[1]   = 0;
+    server->protocol.gameMode.scoreLimit = 10;
+    server->protocol.gameMode.intelFlags = 0;
+    // intel
+    server->protocol.gameMode.intel[0]     = SetIntelTentSpawnPoint(server, 0);
+    server->protocol.gameMode.intel[1]     = SetIntelTentSpawnPoint(server, 1);
+    server->protocol.gameMode.intelHeld[0] = 0;
+    server->protocol.gameMode.intelHeld[1] = 0;
+    // bases
+    server->protocol.gameMode.base[0] = SetIntelTentSpawnPoint(server, 0);
+    server->protocol.gameMode.base[1] = SetIntelTentSpawnPoint(server, 1);
+
+    server->protocol.gameMode.base[0].x = floorf(server->protocol.gameMode.base[0].x);
+    server->protocol.gameMode.base[0].y = floorf(server->protocol.gameMode.base[0].y);
+    server->protocol.gameMode.base[1].x = floorf(server->protocol.gameMode.base[1].x);
+    server->protocol.gameMode.base[1].y = floorf(server->protocol.gameMode.base[1].y);
+}
+
+static void initTC(Server* server)
+{
+    memcpy(server->gamemodeName, "tc", strlen("tc") + 1);
+    printf("GameMode not supported properly yet\n");
+    server->running = 0;
+}
+
+static void initBabel(Server* server)
+{
+    memcpy(server->gamemodeName, "babel", strlen("babel") + 1);
+    printf("GameMode not supported properly yet\n");
+    server->running = 0;
+}
+
+static void initArena(Server* server)
+{
+    memcpy(server->gamemodeName, "arena", strlen("arena") + 1);
+    printf("GameMode not supported properly yet\n");
+    server->running = 0;
+}
 
 void initGameMode(Server* server, uint8 gamemode)
 {
-    if (gamemode == 0) {
-        server->protocol.currentGameMode = GAME_MODE_CTF;
-    } else if (gamemode == 1) {
-        server->protocol.currentGameMode = GAME_MODE_TC;
-    }
+    server->protocol.currentGameMode = gamemode;
 
     if (server->protocol.currentGameMode == GAME_MODE_CTF) {
-        memcpy(server->gamemodeName, "ctf", strlen("ctf") + 1);
-        // Init CTF
-        server->protocol.gameMode.score[0]   = 0;
-        server->protocol.gameMode.score[1]   = 0;
-        server->protocol.gameMode.scoreLimit = 10;
-        server->protocol.gameMode.intelFlags = 0;
-        // intel
-        server->protocol.gameMode.intel[0]     = SetIntelTentSpawnPoint(server, 0);
-        server->protocol.gameMode.intel[1]     = SetIntelTentSpawnPoint(server, 1);
-        server->protocol.gameMode.intelHeld[0] = 0;
-        server->protocol.gameMode.intelHeld[1] = 0;
-        // bases
-        server->protocol.gameMode.base[0] = SetIntelTentSpawnPoint(server, 0);
-        server->protocol.gameMode.base[1] = SetIntelTentSpawnPoint(server, 1);
-
-        server->protocol.gameMode.base[0].x = floorf(server->protocol.gameMode.base[0].x);
-        server->protocol.gameMode.base[0].y = floorf(server->protocol.gameMode.base[0].y);
-        server->protocol.gameMode.base[1].x = floorf(server->protocol.gameMode.base[1].x);
-        server->protocol.gameMode.base[1].y = floorf(server->protocol.gameMode.base[1].y);
+        initCTF(server);
     } else if (server->protocol.currentGameMode == GAME_MODE_TC) {
-        memcpy(server->gamemodeName, "tc", strlen("tc") + 1);
-        printf("GameMode not supported properly yet\n");
-        server->running = 0;
-    }
-    else if (server->protocol.currentGameMode == GAME_MODE_BABEL) {
-        memcpy(server->gamemodeName, "babel", strlen("babel") + 1);
-        printf("GameMode not supported properly yet\n");
-        server->running = 0;
-    }
-    else if (server->protocol.currentGameMode == GAME_MODE_ARENA) {
-        memcpy(server->gamemodeName, "arena", strlen("arena") + 1);
-        printf("GameMode not supported properly yet\n");
-        server->running = 0;
+        initTC(server);
+    } else if (server->protocol.currentGameMode == GAME_MODE_BABEL) {
+        initBabel(server);
+    } else if (server->protocol.currentGameMode == GAME_MODE_ARENA) {
+        initArena(server);
     } else {
-        memcpy(server->gamemodeName, "IDK", strlen("IDK") + 1);
         printf("Unknown GameMode\n");
         server->running = 0;
     }
