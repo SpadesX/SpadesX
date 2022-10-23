11d9a2d29b111b8feb78b20f22287c3a364f1705
sreeshna-nair <110094488+sreeshna-nair@users.noreply.github.com>
2022-10-23 22:11:49 +0530

    Create snakegame.c

---
+++ snakegame.c
@@ -0,0 +1,116 @@
+#include <conio.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+int i, j, height = 20, width = 20;
+int gameover, score;
+int x, y, fruitx, fruity, flag;
+void setup()
+{
+	gameover = 0;
+	x = height / 2;
+	y = width / 2;
+label1:
+	fruitx = rand() % 20;
+	if (fruitx == 0)
+		goto label1;
+label2:
+	fruity = rand() % 20;
+	if (fruity == 0)
+		goto label2;
+	score = 0;
+}
+void draw()
+{
+	system("cls");
+	for (i = 0; i < height; i++) {
+		for (j = 0; j < width; j++) {
+			if (i == 0 || i == width - 1
+				|| j == 0
+				|| j == height - 1) {
+				printf("#");
+			}
+			else {
+				if (i == x && j == y)
+					printf("0");
+				else if (i == fruitx
+						&& j == fruity)
+					printf("*");
+				else
+					printf(" ");
+			}
+		}
+		printf("\n");
+	}
+	printf("score = %d", score);
+	printf("\n");
+	printf("press X to quit the game");
+}
+void input()
+{
+	if (kbhit()) {
+		switch (getch()) {
+		case 'a':
+			flag = 1;
+			break;
+		case 's':
+			flag = 2;
+			break;
+		case 'd':
+			flag = 3;
+			break;
+		case 'w':
+			flag = 4;
+			break;
+		case 'x':
+			gameover = 1;
+			break;
+		}
+	}
+}
+void logic()
+{
+	sleep(0.01);
+	switch (flag) {
+	case 1:
+		y--;
+		break;
+	case 2:
+		x++;
+		break;
+	case 3:
+		y++;
+		break;
+	case 4:
+		x--;
+		break;
+	default:
+		break;
+	}
+	if (x < 0 || x > height
+		|| y < 0 || y > width)
+	if (x == fruitx && y == fruity) {
+	label3:
+		fruitx = rand() % 20;
+		if (fruitx == 0)
+			goto label3;
+	label4:
+		fruity = rand() % 20;
+		if (fruity == 0)
+			goto label4;
+		score += 10;
+	}
+}
+void main()
+{
+	int m, n;
+	setup();
+	while (!gameover) {
+
+		// Function Call
+		draw();
+		input();
+		logic();
+	}
+}
