05da15d4313979c6e662790218da45d9508edf16
Haxk20 <16738302+Haxk20@users.noreply.github.com>
2022-12-26 02:03:00 +0100

    Packets: ShortPlayerData.c: Spawn player after SPD is received

--- Source/Server/Packets/ShortPlayerData.c
+++ Source/Server/Packets/ShortPlayerData.c
@@ -1,4 +1,5 @@
 #include <Server/Packets/Packets.h>
+#include <Server/ParseConvert.h>
 #include "Util/DataStream.h"
 #include "Util/Log.h"
 #include "Util/Uthash.h"
@@ -9,6 +10,9 @@
 
 void receive_short_player(server_t* server, player_t* player, stream_t* data)
 {
+    if (player->team != 255) {
+        return;
+    }
     stream_skip(data, 1); // Sender has to match the player we are getting info of.
     player->team   = stream_read_u8(data);
     player->weapon = stream_read_u8(data);
@@ -39,10 +43,10 @@ void receive_short_player(server_t* server, player_t* player, stream_t* data)
             break;
     }
 
-    if (server->protocol.gamemode.intel_held[0] == 0) {
-        send_move_object(server, 0, 0, server->protocol.gamemode.intel[0]);
-    }
-    if (server->protocol.gamemode.intel_held[1] == 0) {
-        send_move_object(server, 1, 1, server->protocol.gamemode.intel[1]);
-    }
+    player->state = STATE_SPAWNING;
+    char IP[17];
+    format_ip_to_str(IP, player->ip);
+    char team[15];
+    team_id_to_str(server, team, player->team);
+    LOG_INFO("Player %s (%s, #%hhu) joined %s", player->name, IP, player->id, team);
 }
