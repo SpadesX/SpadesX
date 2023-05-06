17f22957aa9ef810f7e1a6ecfb0263e810622e24
utf-4096 <utf16+github@riseup.net>
2023-05-06 14:42:29 +0200

    JSONHelpers.h: Prevent out-of-bounds array access
    Before this we would read outside the bounds of the array if it was
    smaller then expected in the config.
    This uses the values specified in fallback instead.

--- Source/Util/JSONHelpers.h
+++ Source/Util/JSONHelpers.h
@@ -22,18 +22,26 @@
 #define READ_STR_FROM_JSON(pj, var, jsonvar, description, fallback, optional) \
     READ_VAR_FROM_JSON(string, "\"%s\"", pj, var, jsonvar, description, fallback, optional)
 
-#define READ_INT_ARR_FROM_JSON(pj, var, jsonvar, description, fallback, fbsize, optional)            \
-    {                                                                                                \
-        struct json_object* _##var;                                                                  \
-        if (!json_object_object_get_ex(pj, #jsonvar, &_##var)) {                                     \
-            if (optional == 0) {                                                                     \
-                LOG_WARNING("Failed to find " description " in config, falling back to " #fallback); \
-            }                                                                                        \
-            for (int i = 0; i < fbsize; ++i)                                                         \
-                var[i] = fallback[i];                                                                \
-        } else                                                                                       \
-            for (int i = 0; i < fbsize; ++i)                                                         \
-                var[i] = json_object_get_int(json_object_array_get_idx(_##var, i));                  \
+#define READ_INT_ARR_FROM_JSON(pj, var, jsonvar, description, fallback, fbsize, optional)                        \
+    {                                                                                                            \
+        struct json_object* _##var;                                                                              \
+        if (!json_object_object_get_ex(pj, #jsonvar, &_##var)) {                                                 \
+            if (optional == 0) {                                                                                 \
+                LOG_WARNING("Failed to find " description " in config, falling back to " #fallback);             \
+            }                                                                                                    \
+            for (int i = 0; i < fbsize; ++i)                                                                     \
+                var[i] = fallback[i];                                                                            \
+        } else {                                                                                                 \
+            int _len_##var = json_object_array_length(_##var);                                                   \
+            for (int i = 0; i < fbsize; ++i) {                                                                   \
+                if (i < _len_##var) {                                                                            \
+                    var[i] = json_object_get_int(json_object_array_get_idx(_##var, i));                          \
+                } else {                                                                                         \
+                    LOG_WARNING("Config option " #jsonvar "[%i] not found, falling back to %i", i, fallback[i]); \
+                    var[i] = fallback[i];                                                                        \
+                }                                                                                                \
+            }                                                                                                    \
+        }                                                                                                        \
     }
 
 #endif
