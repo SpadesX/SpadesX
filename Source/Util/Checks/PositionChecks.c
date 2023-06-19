cd3cb8a7103e6b0a9d3196d05b1869ac0f3cbabb
utf-4096 <utf16+github@riseup.net>
2023-06-19 16:38:35 +0200

    PositionChecks.c: Refactor small functions
    - `inline`d small functions
    - removed unnecessary if() statements
    - added nan checks using asserts

--- Source/Util/Checks/PositionChecks.c
+++ Source/Util/Checks/PositionChecks.c
@@ -1,85 +1,58 @@
 #include <Server/Structs/ServerStruct.h>
 #include <math.h>
+#include <assert.h>
 
-uint32_t distance_in_3d(vector3f_t vector1, vector3f_t vector2)
+inline uint32_t distance_in_3d(vector3f_t vector1, vector3f_t vector2)
 {
-    uint32_t distance = 0;
-    distance          = sqrt(fabs(pow(vector1.x - vector2.x, 2)) + fabs(pow(vector1.y - vector2.y, 2)) +
-                    fabs(pow(vector1.z - vector2.z, 2)));
+    float distance = sqrt(fabs(pow(vector1.x - vector2.x, 2)) + fabs(pow(vector1.y - vector2.y, 2)) +
+                          fabs(pow(vector1.z - vector2.z, 2)));
+
+    assert(isnan(distance) == 0);
     return distance;
 }
 
-uint32_t distance_in_2d(vector3f_t vector1, vector3f_t vector2)
+inline uint32_t distance_in_2d(vector3f_t vector1, vector3f_t vector2)
 {
-    uint32_t distance = 0;
-    distance          = sqrt(fabs(pow(vector1.x - vector2.x, 2)) + fabs(pow(vector1.y - vector2.y, 2)));
+    float distance = sqrt(fabs(pow(vector1.x - vector2.x, 2)) + fabs(pow(vector1.y - vector2.y, 2)));
+
+    assert(isnan(distance) == 0);
     return distance;
 }
 
-uint8_t collision_3d(vector3f_t vector1, vector3f_t vector2, uint8_t distance)
+inline uint8_t collision_3d(vector3f_t vector1, vector3f_t vector2, uint8_t distance)
 {
-    if (fabs(vector1.x - vector2.x) < distance && fabs(vector1.y - vector2.y) < distance &&
-        fabs(vector1.z - vector2.z) < distance)
-    {
-        return 0;
-    } else {
-        return 1;
-    }
+    return !(fabs(vector1.x - vector2.x) < distance && fabs(vector1.y - vector2.y) < distance &&
+             fabs(vector1.z - vector2.z) < distance);
 }
 
-uint8_t valid_pos_v3i(server_t* server, vector3i_t pos)
+inline uint8_t valid_pos_v3i(server_t* server, vector3i_t pos)
 {
-    if (pos.x < server->s_map.map.size_x && pos.x >= 0 && pos.y < server->s_map.map.size_y && pos.y >= 0 &&
-        pos.z < server->s_map.map.size_z && pos.z >= 0)
-    {
-        return 1;
-    } else {
-        return 0;
-    }
+    return pos.x < server->s_map.map.size_x && pos.x >= 0 && pos.y < server->s_map.map.size_y && pos.y >= 0 &&
+           pos.z < server->s_map.map.size_z && pos.z >= 0;
 }
 
-uint8_t valid_pos_v3f(server_t* server, vector3f_t pos)
+inline uint8_t valid_pos_v3f(server_t* server, vector3f_t pos)
 {
-    if (pos.x < server->s_map.map.size_x && pos.x >= 0 && pos.y < server->s_map.map.size_y && pos.y >= 0 &&
-        pos.z < server->s_map.map.size_z && pos.z >= 0)
-    {
-        return 1;
-    } else {
-        return 0;
-    }
+    return pos.x < server->s_map.map.size_x && pos.x >= 0 && pos.y < server->s_map.map.size_y && pos.y >= 0 &&
+           pos.z < server->s_map.map.size_z && pos.z >= 0;
 }
 
-uint8_t valid_pos_v3f_below_z(server_t* server, vector3f_t pos)
+inline uint8_t valid_pos_v3f_below_z(server_t* server, vector3f_t pos)
 {
-    if (pos.x < server->s_map.map.size_x && pos.x >= 0 && pos.y < server->s_map.map.size_y && pos.y >= 0 &&
-        pos.z < server->s_map.map.size_z && pos.z >= -4)
-    {
-        return 1;
-    } else {
-        return 0;
-    }
+    return pos.x < server->s_map.map.size_x && pos.x >= 0 && pos.y < server->s_map.map.size_y && pos.y >= 0 &&
+           pos.z < server->s_map.map.size_z && pos.z >= -4;
 }
 
-uint8_t valid_pos_3i(server_t* server, int x, int y, int z)
+inline uint8_t valid_pos_3i(server_t* server, int x, int y, int z)
 {
-    if (x < server->s_map.map.size_x && x >= 0 && y < server->s_map.map.size_y && y >= 0 &&
-        z < server->s_map.map.size_z && z >= 0)
-    {
-        return 1;
-    } else {
-        return 0;
-    }
+    return x < server->s_map.map.size_x && x >= 0 && y < server->s_map.map.size_y && y >= 0 &&
+           z < server->s_map.map.size_z && z >= 0;
 }
 
-uint8_t valid_pos_3f(server_t* server, float x, float y, float z)
+inline uint8_t valid_pos_3f(server_t* server, float x, float y, float z)
 {
-    if (x < server->s_map.map.size_x && x >= 0 && y < server->s_map.map.size_y && y >= 0 &&
-        z < server->s_map.map.size_z && z >= 0)
-    {
-        return 1;
-    } else {
-        return 0;
-    }
+    return x < server->s_map.map.size_x && x >= 0 && y < server->s_map.map.size_y && y >= 0 &&
+           z < server->s_map.map.size_z && z >= 0;
 }
 
 uint8_t valid_player_pos(server_t* server, player_t* player, float X, float Y, float Z)
