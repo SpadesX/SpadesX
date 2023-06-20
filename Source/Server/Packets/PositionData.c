f8d7b608288663cb080a29679531ea8cf2d01454
utf-4096 <utf16+github@riseup.net>
2023-06-20 13:32:31 +0200

    PositionData.c: Check if player is in solid blocks
    This adds a check to discard PositionData packets if the
    player is covered in solid blocks (from feet to head),
    as movement is normally impossible in this state.

--- Source/Server/Packets/PositionData.c
+++ Source/Server/Packets/PositionData.c
@@ -22,7 +22,7 @@ void receive_position_data(server_t* server, player_t* player, stream_t* data)
 {
     vector3f_t position = {stream_read_f(data), stream_read_f(data), stream_read_f(data)};
 
-    if(!valid_vec3f(position)) {
+    if (!valid_vec3f(position)) {
         return;
     }
 
@@ -30,6 +30,18 @@ void receive_position_data(server_t* server, player_t* player, stream_t* data)
         send_position_packet(
         server, player, player->movement.position.x, player->movement.position.y, player->movement.position.z);
         return;
+    }
+
+    if (player->movement.position.z > 0) {
+        uint16_t x = player->movement.position.x;
+        uint16_t y = player->movement.position.y;
+        uint16_t z = player->movement.position.z;
+
+        if (z <= server->s_map.map.size_z - 2 && mapvxl_is_solid(&server->s_map.map, x, y, z) &&
+            mapvxl_is_solid(&server->s_map.map, x, y, z + 1) && mapvxl_is_solid(&server->s_map.map, x, y, z + 2))
+        {
+            return;
+        }
     }
 
     player->movement.prev_position = player->movement.position;
